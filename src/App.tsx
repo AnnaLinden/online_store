8dcb30fd833396c4a442d154c01c3ff10412fa59
Anna Linden <linden.a.anna@gmail.com>
2025-01-02 21:37:44 +0200

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
-    <h1 className="text-7xl font-bold">
+    <div>
+      <h1 className="text-7xl font-bold">
       App
     </h1>
+    <Button
+    variant='destructive'
+    size='lg'
+    onClick={() => console.log('it worked!!!')}
+  >
+    Click Me
+  </Button>
+  </div>
   )
 }
 
