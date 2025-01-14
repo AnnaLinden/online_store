acb1c5805f1cff8b22cca07976b393eea4bd17d9
Anna Linden <linden.a.anna@gmail.com>
2025-01-14 11:33:31 +0200

    Link Component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 function Cart() {
-    return <h1 className='text-4xl'>Cart Page</h1>;
-  }
+    return 
+    <div>
+    <h1 className='text-4xl'>Cart Page</h1>;
+    <Link to='/' className="text-7xl text-red-900">
+        Back Home
+    </Link> 
+    <Button asChild size="lg">
+        <Link to='/'>Home Button</Link>
+    </Button>
+    </div>
+  };
   export default Cart;
