83bca9af332267bb484ab75f4ed05270647f1430
Anna Linden <linden.a.anna@gmail.com>
2025-01-14 10:04:46 +0200

    pages created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
+
 import { Button } from './components/ui/button';
 import { useAppSelector } from './hooks';
 
 function App() {
   const { name } = useAppSelector((state) => state.userState);
   console.log(name);
-  
+
   return (
     <div>
       <h1 className="text-7xl font-bold">
