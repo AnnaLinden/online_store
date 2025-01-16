83affc597c3b3b9edb630f83ff4ce0c7d850c2ff
Anna Linden <linden.a.anna@gmail.com>
2025-01-16 08:59:09 +0200

    icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import { Wheat } from "lucide-react";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Link
+      to='/'
+      className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-w>
+   text-white'
+    >
+      <Wheat className='w-8 h-8' />
+    </Link>
+  );
 }
 
 export default Logo;
