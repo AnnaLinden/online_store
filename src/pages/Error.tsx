81617b95a93ed2aa955f804fa1cc3e3ac23017ca
Anna Linden <linden.a.anna@gmail.com>
2025-01-18 11:10:42 +0200

    error page component

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,27 @@
-function Error(){
-    return <h1 className="text-4xl">Error Page</h1>
+import { useRouteError, Link, isRouteErrorResponse } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+function Error() {
+  const error = useRouteError();
+  console.log(error);
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    <main className='grid min-h-[100vh] place-items-center px-8 '>
+      <div className='text-center'>
+        <p className='text-9xl font-semibold text-primary'>404</p>
+        <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+          Page not found
+        </h1>
+        <p className='mt-6 text-lg leading-7'>
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className='mt-10 '>
+          <Button asChild size='lg' variant='secondary'>
+            <Link to='/'>Go back home</Link>
+          </Button>
+        </div>
+      </div>
+    </main>;
+  }
+  return <h1 className='text-4xl'>Error Page</h1>;
 }
 export default Error;
