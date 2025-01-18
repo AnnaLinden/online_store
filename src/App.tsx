f41000d13dff2f1034f6a0bcc97b9cec3982ae61
Anna Linden <linden.a.anna@gmail.com>
2025-01-18 11:39:59 +0200

    error element

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
   Checkout,
   Orders,
 } from "./pages";
+import { ErrorElement } from "./components";
 
 const router = createBrowserRouter([
   {
@@ -24,27 +25,33 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "products",
         element: <Products />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "cart",
         element: <Cart />,
+        errorElement: <ErrorElement />,
       },
-      { path: "about", element: <About /> },
+      { path: "about", element: <About />, errorElement: <ErrorElement /> },
       {
         path: "checkout",
         element: <Checkout />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "orders",
         element: <Orders />,
+        errorElement: <ErrorElement />,
       },
     ],
   },
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,4 @@
 function Products() {
-  something;
   return <h1 className='text-4xl'>Products Page</h1>;
 }
 export default Products;
