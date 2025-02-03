20eb3c0c7090ad6d0b4d97ed3eed3fb6a0ea76be
Anna Linden <linden.a.anna@gmail.com>
2025-02-03 14:46:40 +0200

    products page setup

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
 } from "./pages";
 import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
+import { loader as productsLoader } from "./pages/Products";
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,7 @@ const router = createBrowserRouter([
       {
         path: "products",
         element: <Products />,
+        loader: productsLoader,
         errorElement: <ErrorElement />,
       },
       {
--- src/components/index.ts
+++ src/components/index.ts
@@ -14,4 +14,4 @@ export { default as ProductsGrid } from "./ProductsGrid";
 export { default as Filters } from "./Filters";
 export { default as ProductsContainer } from "./ProductsContainer";
 export { default as PaginationContainer } from "./PaginationContainer";
-export { default as Products } from "./Products";
+export { default as ProductsList } from "./ProductsList";
