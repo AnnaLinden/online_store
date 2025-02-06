84d49e2a10fd85011cb454f46c415c8c2d52934c
Anna Linden <linden.a.anna@gmail.com>
2025-02-06 10:15:36 +0200

    wuery params

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,8 +4,14 @@ import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
 
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url);
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponse> => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
+  const response = await customFetch<ProductsResponse>(url, { params });
   console.log(response);
 
   return { ...response.data };
