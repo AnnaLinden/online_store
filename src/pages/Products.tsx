16efde39b4de628ccfd1643b85ba0279bb8a0e5f
Anna Linden <linden.a.anna@gmail.com>
2025-02-06 10:28:10 +0200

    default value

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,20 +1,26 @@
 import { Filters, ProductsContainer, PaginationContainer } from "@/components";
-import { customFetch, type ProductsResponse } from "@/utils";
+import {
+  customFetch,
+  type ProductsResponse,
+  type ProductsResponseWithParams,
+} from "@/utils";
 import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
 
 export const loader: LoaderFunction = async ({
   request,
-}): Promise<ProductsResponse> => {
+}): Promise<ProductsResponseWithParams> => {
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
-  console.log(params);
-  const response = await customFetch<ProductsResponse>(url, { params });
+
+  const response = await customFetch<ProductsResponse>(url, {
+    params,
+  });
   console.log(response);
 
-  return { ...response.data };
+  return { ...response.data, params };
 };
 
 function Products() {
