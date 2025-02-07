149444960d4d403ef8fe7902d7cf8a4377ad1fe4
Anna Linden <linden.a.anna@gmail.com>
2025-02-07 15:54:04 +0200

    index and filters fixed

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,4 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
 import { ProductsResponseWithParams } from "@/utils";
 import FormInput from "./FormInput";
@@ -11,34 +9,21 @@ function Filters() {
   const { search, company, category, shipping, order, price } = params;
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      <div className='mb-2'>
-        <Label htmlFor='search'>Search Product</Label>
-        <Input id='search' name='search' type='text' defaultValue={search} />
-      </div>
-      <Button type='submit' size='sm' className='self-end mb-2'>
-        search
-      </Button>
-      <Button
-        type='button'
-        asChild
-        size='sm'
-        variant='outline'
-        className='self-end mb-2'
-      >
-        <Link to='/products'>reset</Link>
-      </Button>
+      {/*SEARCH */}
       <FormInput
         type='search'
         label='search product'
         name='search'
         defaultValue={search}
       />
+      {/*CATGORIES */}
       <FormSelect
         label='select category'
         name='category'
         options={meta.categories}
         defaultValue={category}
       />
+
       {/* COMPANIES */}
       <FormSelect
         label='select company'
@@ -53,6 +38,19 @@ function Filters() {
         options={["a-z", "z-a", "high", "low"]}
         defaultValue={order}
       />
+
+      <Button type='submit' size='sm' className='self-end mb-2'>
+        search
+      </Button>
+      <Button
+        type='button'
+        asChild
+        size='sm'
+        variant='outline'
+        className='self-end mb-2'
+      >
+        <Link to='/products'>reset</Link>
+      </Button>
     </Form>
   );
 }
