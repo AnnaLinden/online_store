d592b957b80cfa4d7081b0869902e5fd15519cfc
Anna Linden <linden.a.anna@gmail.com>
2025-02-13 20:39:40 +0200

    select product color

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
 import { type LoaderFunction } from "react-router-dom";
+import SelectProductColor from "@/components/SelectProductColor";
 
 export const loader: LoaderFunction = async ({
   params,
@@ -27,6 +28,49 @@ const SingleProduct = () => {
   const addToCart = () => {
     console.log("add to cart");
   };
-  return <h1 className='text-4xl'>SingleProduct Page</h1>;
+  return (
+    <section>
+      <div className='flex gap-x-2 h-6 items-center'>
+        <Button asChild variant='link' size='sm'>
+          <Link to='/'>Home</Link>
+        </Button>
+        <Separator orientation='vertical' />
+        <Button asChild variant='link' size='sm'>
+          <Link to='/products'>Products</Link>
+        </Button>
+      </div>
+      {/* PRODUCT */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className='w-96 h-96 object-cover rounded-lg lg:w-full'
+        />
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl mt-2'>{company}</h4>
+          <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
+            {euroAmount}
+          </p>
+          <p className='mt-6 leading-8'>{description}</p>
+          {/* COLORS */}
+          <SelectProductColor
+            colors={colors}
+            productColor={productColor}
+            setProductColor={setProductColor}
+          />
+
+          {/* AMOUNT */}
+
+          {/* CART BUTTON */}
+          <Button size='lg' className='mt-10' onClick={addToCart}>
+            Add to bag
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default SingleProduct;
