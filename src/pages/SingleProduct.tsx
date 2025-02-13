092abec311f9f0b0926f68e4537659dae8580c7c
Anna Linden <linden.a.anna@gmail.com>
2025-02-13 20:19:53 +0200

    fetch single product

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,22 +1,32 @@
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
-import {
-  customFetch,
-  formatAsDollars,
-  type SingleProductResponse,
-} from "@/utils";
+import { customFetch, formatAsEuro, type SingleProductResponse } from "@/utils";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
 import { type LoaderFunction } from "react-router-dom";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  console.log(params);
-  return null;
+export const loader: LoaderFunction = async ({
+  params,
+}): Promise<SingleProductResponse> => {
+  const response = await customFetch<SingleProductResponse>(
+    `/products/${params.id}`
+  );
+  return { ...response.data };
 };
 
-function SingleProduct() {
+const SingleProduct = () => {
+  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const euroAmount = formatAsEuro(price);
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const addToCart = () => {
+    console.log("add to cart");
+  };
   return <h1 className='text-4xl'>SingleProduct Page</h1>;
-}
+};
 export default SingleProduct;
