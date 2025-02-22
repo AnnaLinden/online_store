159e7ff2a073ac7f6875db2e84dfe5ffd6d707c4
Anna Linden <linden.a.anna@gmail.com>
2025-02-22 21:21:51 +0200

    add item reducer

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,6 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { type CartItem, type CartState } from "@/utils";
 import { toast } from "@/components/ui/use-toast";
+import { stringify } from "querystring";
+import { Description } from "@radix-ui/react-toast";
 
 const defaultState: CartState = {
   cartItems: [],
@@ -20,11 +22,30 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: getCartFromLocalStorage(),
   reducers: {
-    addItem: () => {},
+    addItem: (state, action: PayloadAction<CartItem>) => {
+      const newCartItem = action.payload;
+      const item = state.cartItems.find((i) => i.cartID === newCartItem.cartID);
+      if (item) {
+        item.amount += newCartItem.amount;
+      } else {
+        state.cartItems.push(newCartItem);
+      }
+      state.numItemsInCart += newCartItem.amount;
+      state.cartTotal += Number(newCartItem.price) * newCartItem.amount;
+      //   state.tax = 0.1 * state.cartTotal;
+      //   state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      //   localStorage.setItem("cart", JSON.stringify(state));
+      cartSlice.caseReducers.calculateTotal(state);
+      toast({ description: "Item added to cart" });
+    },
     clearCart: () => {},
     removeItem: () => {},
     editItem: () => {},
-    calculateTotal: () => {},
+    calculateTotal: (state) => {
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
   },
 });
 export const { addItem, clearCart, removeItem, editItem } = cartSlice.actions;
