780e0ebc0737c88763ecf15c1f08825c714b9c9c
Anna Linden <linden.a.anna@gmail.com>
2025-02-24 09:54:50 +0200

    cart items list

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -1,5 +1,21 @@
-function CartItemColumns() {
-  return <div></div>;
-}
+import { formatAsEuro } from "@/utils";
+import { useAppDispatch } from "@/hooks";
+import { Button } from "./ui/button";
+import { editItem, removeItem } from "@/features/cart/cartSlice";
+import SelectProductAmount from "./SelectProductAmount";
+import { Mode } from "./SelectProductAmount";
 
-export default CartItemColumns;
+export const FirstColumn = () => {
+  return <h4>First Column</h4>;
+};
+export const SecondColumn = () => {
+  return <h4>Second Column</h4>;
+};
+
+export const ThirdColumn = () => {
+  return <h4>Third Column</h4>;
+};
+
+export const FourthColumn = () => {
+  return <h4>Fourth Column</h4>;
+};
